41a6615d35c85da644e850d6a9a1ac60c7ce80c9
Rauan <rauan_pires@riseup.net>
2018-10-23 03:12:34 -0300

    Fix

--- main.cpp
+++ main.cpp
@@ -45,7 +45,7 @@ int main(int argc, char *argv[]){
 
         frameDraw = frame.clone();
 
-        //medianBlur(diff, diff, 5);
+        
 
         if (frame.empty())
             break;
@@ -74,7 +74,7 @@ int main(int argc, char *argv[]){
 
         for (int r = 1; r < 3; r++)
         {
-            Mat kernel = getStructuringElement(MORPH_ELLIPSE, Size(4*r + 1, 4*r +1));
+            Mat kernel = getStructuringElement(MORPH_ELLIPSE, Size(5*r + 1, 5*r +1));
             morphologyEx(diff, diff, MORPH_CLOSE, kernel);
             morphologyEx(diff, diff, MORPH_OPEN, kernel);
         }
@@ -84,7 +84,6 @@ int main(int argc, char *argv[]){
        // dilate(diff, diff, 0, Point(-1, -1), 8, 1, 1);
         vector<vector< Point > >conts;
         vector<vector< Point > >contsHull;
-        vector<vector< Point > >contsHull2;
         vector<Vec4i> hierarchy;
 
 
@@ -98,7 +97,7 @@ int main(int argc, char *argv[]){
 //faz o hull dos contornos iniciais
         for(int i=0; i<conts.size(); i++){
 
-            if(conts[i].size() > 150 && hierarchy[i][3] == -1){   // se for maior e nao tiver pai (coitado)
+            if(conts[i].size() > 180 && hierarchy[i][3] == -1){   // se for maior e nao tiver pai (coitado)
 
 
                 convexHull( Mat(conts[i]), hull[i], false );
@@ -107,52 +106,14 @@ int main(int argc, char *argv[]){
                 
             }
 
-        }
-// calcula momentos de cada contorno dps do hull se < 100     
+        } 
         
-        int numMom = 0;
-        findContours(diff, contsHull, RETR_LIST  , CHAIN_APPROX_NONE);
-        vector<Moments> mom(contsHull.size());
-        for(int i=0; i<contsHull.size(); i++){
-            if(contsHull[i].size() > 1){
-
-                mom[numMom] = moments(contsHull[i],true);
-                numMom++;
-
-            }
-        }
-// pra cada momento faz o centro e calcula a distancia pros outros e desenha se perto
-        for(int i =0 ; i < numMom;i++){
-            Point p(mom[i].m10/mom[i].m00, mom[i].m01/mom[i].m00);
-            for(int j = i; j < numMom; j++){
-                Point p2(mom[j].m10/mom[j].m00, mom[j].m01/mom[j].m00);
-                double dist  = norm(p-p2);
-                if( dist < 30){
-                    line(diff, p, p2, cv::Scalar(255,255,255), 4);
-                }
-            }
-        }
-
-// //acha o novo contorno e faz hull dnv
-//         findContours(diff, contsHull, RETR_LIST  , CHAIN_APPROX_NONE);
-//         for(int i=0; i<contsHull.size(); i++){
-
-//             if(contsHull[i].size() > 150){   // se for maior e nao tiver pai (coitado)
-
-
-//                 convexHull( Mat(conts[i]), hull[i], false );
-
-//                 drawContours( diff , hull, i, Scalar(255,255,255), -1, 8, vector<Vec4i>(), 0, Point() );
-                
-//             }
-
-//         }
         findContours(diff, contsHull, RETR_LIST  , CHAIN_APPROX_NONE);
         for(int i=0; i<contsHull.size(); i++){
-            if(contsHull[i].size() > 100){
+            if(contsHull[i].size() > 200){
 
 
-                
+
                 Mat M;
 
                 Rect boundRect = boundingRect(Mat(contsHull[i]));
